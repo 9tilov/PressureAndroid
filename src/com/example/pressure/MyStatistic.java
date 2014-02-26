40a9f0cc65b4e51ee2493be7ca81740de94c097e
dmitry <dmitry@dmitry-moggot.(none)>
2014-02-27 02:39:28 +0400

    Добавил вывод имени профиля по id

--- src/com/example/pressure/MyStatistic.java
+++ src/com/example/pressure/MyStatistic.java
@@ -6,13 +6,25 @@ import android.widget.TextView;
 
 public class MyStatistic extends Activity {
 	private TextView name;
+	MyDB db;
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistic);
+        
+        db = new MyDB(this);
+		db.open();
+		
         name = (TextView) findViewById(R.id.profile_name);
-        String profile_name = getIntent().getStringExtra("lvData");
+        String profile_id = getIntent().getStringExtra("lvData");
+        String profile_name = db.getCurrentName(Long.parseLong(profile_id));
         name.setText(profile_name);
     }
+    
+    protected void onDestroy() {
+		super.onDestroy();
+		// закрываем подключение при выходе
+		db.close();
+	}
 }
