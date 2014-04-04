1804720a628f9cd44251a76ce13d57f207a11d7f
moggot <9.tilov@gmail.com>
2014-04-04 18:31:38 +0400

    переключение между графиками с помощью вкладок

--- src/com/example/pressure/Graph.java
+++ src/com/example/pressure/Graph.java
@@ -1,25 +1,15 @@
 package com.example.pressure;
 
-import java.util.LinkedList;
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphView.GraphViewData;
-import com.jjoe64.graphview.GraphView.LegendAlign;
-import com.jjoe64.graphview.GraphViewSeries.GraphViewSeriesStyle;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.LineGraphView;
-
-import android.view.KeyEvent;
-import android.view.View;
+import android.app.TabActivity;
 import android.os.Bundle;
-import android.app.Activity;
+import android.widget.TabHost;
+
 import android.content.Intent;
-import android.graphics.Color;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.view.View.OnClickListener;
 
-public class Graph extends Activity implements OnClickListener {
+public class Graph extends TabActivity {
 
 	String stat_id;
 	String count_data_string;
@@ -45,18 +35,46 @@ public class Graph extends Activity implements OnClickListener {
 		Log.d(LOG_TAG, "stat_id= " + stat_id);
 		db = new MyDB(this);
 		db.open();
+		TabHost tabHost = getTabHost();
 
-		createGraph(Integer.valueOf(count_data_string));
+		TabHost.TabSpec tabSpec;
 
-		btnWeek = (Button) findViewById(R.id.btnWeek);
-		btnMonth = (Button) findViewById(R.id.btnMonth);
-		btn3Month = (Button) findViewById(R.id.btn3Month);
-		btnAll = (Button) findViewById(R.id.btnAll);
+		Intent intentWeek = new Intent(Graph.this, GraphWeek.class);
+		Intent intentMonth = new Intent(Graph.this, GraphMonth.class);
+		Intent intent3Month = new Intent(Graph.this, Graph3Month.class);
+		Intent intentAllPeriod = new Intent(Graph.this, GraphAllPeriod.class);
 
-		btnWeek.setOnClickListener(this);
-		btnMonth.setOnClickListener(this);
-		btn3Month.setOnClickListener(this);
-		btnAll.setOnClickListener(this);
+		tabSpec = tabHost.newTabSpec("tag1");
+		tabSpec.setIndicator("all period");
+		intentAllPeriod.putExtra("id_stat_key_all_period", stat_id);
+		intentAllPeriod.putExtra("id_stat_count_all_period",
+				String.valueOf(count_data_string));
+		tabSpec.setContent(intentAllPeriod);
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost.newTabSpec("tag2");
+		tabSpec.setIndicator("week");
+		intentWeek.putExtra("id_stat_key_week", stat_id);
+		intentWeek.putExtra("id_stat_count_week",
+				String.valueOf(count_data_string));
+		tabSpec.setContent(intentWeek);
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost.newTabSpec("tag3");
+		tabSpec.setIndicator("month");
+		intentMonth.putExtra("id_stat_key_month", stat_id);
+		intentMonth.putExtra("id_stat_count_month",
+				String.valueOf(count_data_string));
+		tabSpec.setContent(intentMonth);
+		tabHost.addTab(tabSpec);
+
+		tabSpec = tabHost.newTabSpec("tag4");
+		tabSpec.setIndicator("3 months");
+		intent3Month.putExtra("id_stat_key_3_month", stat_id);
+		intent3Month.putExtra("id_stat_count_3_month",
+				String.valueOf(count_data_string));
+		tabSpec.setContent(intent3Month);
+		tabHost.addTab(tabSpec);
 
 	}
 
@@ -66,86 +84,4 @@ public class Graph extends Activity implements OnClickListener {
 		super.onDestroy();
 	}
 
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.btnWeek:
-			period = 7;
-			if (period <= Integer.valueOf(count_data_string))
-				createGraph(period);
-			else
-				createGraph(Integer.valueOf(count_data_string));
-			break;
-		case R.id.btnMonth:
-			period = 30;
-			if (period <= Integer.valueOf(count_data_string))
-				createGraph(period);
-			else
-				createGraph(Integer.valueOf(count_data_string));
-			break;
-		case R.id.btn3Month:
-			period = 90;
-			if (period <= Integer.valueOf(count_data_string))
-				createGraph(period);
-			else
-				createGraph(Integer.valueOf(count_data_string));
-			break;
-		case R.id.btnAll:
-			createGraph(Integer.valueOf(count_data_string));
-			break;
-		}
-	}
-
-	public void createGraph(int period) {
-		LinearLayout layout = (LinearLayout) findViewById(R.id.graph1);
-		layout.removeView(graphView);
-
-		graphView = new LineGraphView(this // context
-				, "Pressure statistics" // heading
-		);
-
-		LinkedList<String[]> list = new LinkedList<String[]>();
-		list = db.getStat(Long.valueOf(stat_id),
-				Integer.valueOf(count_data_string), period);
-
-		int count = Integer.valueOf(period);
-		GraphViewData[] dataPulse = new GraphViewData[count];
-		GraphViewData[] dataSys = new GraphViewData[count];
-		GraphViewData[] dataDias = new GraphViewData[count];
-
-		for (int i = 0; i < count; ++i) {
-			dataPulse[i] = new GraphViewData(i + 1,
-					Double.valueOf(list.get(0)[i]));
-			dataSys[i] = new GraphViewData(i + 1,
-					Double.valueOf(list.get(1)[i]));
-			dataDias[i] = new GraphViewData(i + 1,
-					Double.valueOf(list.get(2)[i]));
-		}
-
-		GraphViewSeriesStyle stylePulse = new GraphViewSeriesStyle();
-		stylePulse.color = Color.rgb(90, 250, 0);
-		GraphViewSeries seriesPulse = new GraphViewSeries("Pulse", stylePulse,
-				dataPulse);
-
-		GraphViewSeriesStyle styleSys = new GraphViewSeriesStyle();
-		styleSys.color = Color.rgb(200, 50, 0);
-		GraphViewSeries seriesSys = new GraphViewSeries("Sys.", styleSys,
-				dataSys);
-
-		GraphViewSeriesStyle styleDias = new GraphViewSeriesStyle();
-		styleDias.color = Color.rgb(300, 50, 160);
-		GraphViewSeries seriesDias = new GraphViewSeries("Dias.", styleDias,
-				dataDias);
-
-		graphView.addSeries(seriesSys);
-		graphView.addSeries(seriesDias);
-		graphView.addSeries(seriesPulse);
-
-		graphView.setShowLegend(true);
-
-		graphView.getGraphViewStyle().setNumVerticalLabels(6);
-		graphView.setLegendAlign(LegendAlign.TOP);
-		graphView.setLegendWidth(130);
-
-		layout.addView(graphView);
-	}
 }
---
+++ src/com/example/pressure/Graph3Month.java
@@ -0,0 +1,108 @@
+package com.example.pressure;
+
+import java.util.LinkedList;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphView.LegendAlign;
+import com.jjoe64.graphview.GraphViewSeries.GraphViewSeriesStyle;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class Graph3Month extends Activity {
+
+	String stat_id;
+	String count_data_string;
+	GraphView graphView;
+	int period = 0;
+
+	final String LOG_TAG = "myLogs";
+
+	MyDB db;
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.month_3_graph);
+		stat_id = getIntent().getStringExtra("id_stat_key_3_month");
+		count_data_string = getIntent().getStringExtra("id_stat_count_3_month");
+
+		Log.d(LOG_TAG, "stat_idssss = " + stat_id);
+		Log.d(LOG_TAG, "count_data_stringssss = " + count_data_string);
+		LinearLayout layout = (LinearLayout) findViewById(R.id.graph3Month);
+		layout.removeView(graphView);
+
+		graphView = new LineGraphView(this // context
+				, "Pressure statistics" // heading
+		);
+		db = new MyDB(this);
+		db.open();
+		period = 16;
+		if (period <= Integer.valueOf(count_data_string)) {
+			LinkedList<String[]> list = new LinkedList<String[]>();
+			list = db.getStat(Long.valueOf(stat_id),
+					Integer.valueOf(count_data_string), period);
+
+			int count = Integer.valueOf(period);
+			GraphViewData[] dataPulse = new GraphViewData[count];
+			GraphViewData[] dataSys = new GraphViewData[count];
+			GraphViewData[] dataDias = new GraphViewData[count];
+
+			for (int i = 0; i < count; ++i) {
+				dataPulse[i] = new GraphViewData(i + 1, Double.valueOf(list
+						.get(0)[i]));
+				dataSys[i] = new GraphViewData(i + 1, Double.valueOf(list
+						.get(1)[i]));
+				dataDias[i] = new GraphViewData(i + 1, Double.valueOf(list
+						.get(2)[i]));
+			}
+
+			GraphViewSeriesStyle stylePulse = new GraphViewSeriesStyle();
+			stylePulse.color = Color.rgb(90, 250, 0);
+			GraphViewSeries seriesPulse = new GraphViewSeries("Pulse",
+					stylePulse, dataPulse);
+
+			GraphViewSeriesStyle styleSys = new GraphViewSeriesStyle();
+			styleSys.color = Color.rgb(200, 50, 0);
+			GraphViewSeries seriesSys = new GraphViewSeries("Sys.", styleSys,
+					dataSys);
+
+			GraphViewSeriesStyle styleDias = new GraphViewSeriesStyle();
+			styleDias.color = Color.rgb(300, 50, 160);
+			GraphViewSeries seriesDias = new GraphViewSeries("Dias.",
+					styleDias, dataDias);
+
+			graphView.addSeries(seriesSys);
+			graphView.addSeries(seriesDias);
+			graphView.addSeries(seriesPulse);
+
+			graphView.setShowLegend(true);
+
+			graphView.getGraphViewStyle().setNumVerticalLabels(6);
+			graphView.setLegendAlign(LegendAlign.TOP);
+			graphView.setLegendWidth(130);
+
+			layout.addView(graphView);
+		} else
+			notEnoughRecords();
+
+	}
+
+	protected void onDestroy() {
+		// закрываем подключение при выходе
+		db.close();
+		super.onDestroy();
+	}
+
+	void notEnoughRecords() {
+		Toast.makeText(this, R.string.not_enough_records, Toast.LENGTH_SHORT)
+				.show();
+	}
+}
+
