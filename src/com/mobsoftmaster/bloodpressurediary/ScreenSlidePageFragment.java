888f019f8b1fe9b57f0ea15721e5194d901eec41
sermobile <ssryabov@gmail.com>
2014-07-27 02:35:09 +0400

    * добавил корерктный вывод картинки в зависимости от разрешения экрана * устройства;

--- src/com/mobsoftmaster/bloodpressurediary/ScreenSlidePageFragment.java
+++ src/com/mobsoftmaster/bloodpressurediary/ScreenSlidePageFragment.java
@@ -16,16 +16,26 @@
 
 package com.mobsoftmaster.bloodpressurediary;
 
+import android.annotation.TargetApi;
 import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import java.util.Locale;
+
+import android.content.Context;
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 
 /**
  * A fragment representing a single step in a wizard. The fragment shows a dummy
@@ -72,8 +82,8 @@ public class ScreenSlidePageFragment extends Fragment {
 		return fragment;
 	}
 
-	public ScreenSlidePageFragment() {
-	}
+//	public ScreenSlidePageFragment() {
+//	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -81,6 +91,26 @@ public class ScreenSlidePageFragment extends Fragment {
 		mPageNumber = getArguments().getInt(ARG_PAGE);
 	}
 
+	public class ScreenResolution {
+		int width;
+		int height;
+	}
+	
+	private ScreenResolution GetResolution() {
+		ScreenResolution sr = new ScreenResolution();
+		Display display = getActivity().getWindowManager().getDefaultDisplay();
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			Point size = new Point();
+			display.getSize(size);
+			sr.width = size.x;
+			sr.height = size.y;
+		} else {
+			sr.width = display.getWidth();
+			sr.height = display.getHeight();
+		}
+		return sr;
+	}
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -93,6 +123,9 @@ public class ScreenSlidePageFragment extends Fragment {
 		final ImageView screen = (ImageView) rootView
 				.findViewById(R.id.imageViewScreenProfile);
 
+		ScreenResolution sr = GetResolution();
+		final int width = sr.width;
+		final int height = sr.height;
 		myOrientationEventListener = new OrientationEventListener(getActivity()) {
 
 			@Override
@@ -120,7 +153,9 @@ public class ScreenSlidePageFragment extends Fragment {
 
 					}
 				}
-				screen.setImageResource(resID);
+//				screen.setImageResource(resID);
+				screen.setImageBitmap(
+					    decodeSampledBitmapFromResource(getResources(), resID, width/2, height/2));
 			}
 		};
 
@@ -147,7 +182,12 @@ public class ScreenSlidePageFragment extends Fragment {
 							getActivity().getPackageName());
 				}
 			}
-			screen.setImageResource(resID);
+//			screen.setImageResource(resID);
+			
+
+			screen.setImageBitmap(
+				    decodeSampledBitmapFromResource(getResources(), resID, width/2, height/2));
+			
 			myOrientationEventListener.enable();
 
 			if (mPageNumber == 8) {
@@ -158,6 +198,44 @@ public class ScreenSlidePageFragment extends Fragment {
 		return rootView;
 	}
 
+	public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    // Raw height and width of image
+    final int height = options.outHeight;
+    final int width = options.outWidth;
+    int inSampleSize = 1;
+
+    if (height > reqHeight || width > reqWidth) {
+
+        final int halfHeight = height / 2;
+        final int halfWidth = width / 2;
+
+        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+        // height and width larger than the requested height and width.
+        while ((halfHeight / inSampleSize) > reqHeight
+                && (halfWidth / inSampleSize) > reqWidth) {
+            inSampleSize *= 2;
+        }
+    }
+
+    return inSampleSize;
+	}
+	
+	public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+	        int reqWidth, int reqHeight) {
+
+	    // First decode with inJustDecodeBounds=true to check dimensions
+	    final BitmapFactory.Options options = new BitmapFactory.Options();
+	    options.inJustDecodeBounds = true;
+	    BitmapFactory.decodeResource(res, resId, options);
+
+	    // Calculate inSampleSize
+	    options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+	    // Decode bitmap with inSampleSize set
+	    options.inJustDecodeBounds = false;
+	    return BitmapFactory.decodeResource(res, resId, options);
+	}
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
